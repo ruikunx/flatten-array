73d28bc5876ec7f4459537da345e2e2028b9c55a
Ruikun Xu <ruikun.xu@booking.com>
2017-05-02 15:23:31 +0200

    Flatten array functions

---
+++ flattenArray.js
@@ -0,0 +1,14 @@
+function flattenArray_ES5(arr) {
+  var new_arr = [];
+  for (var i = 0; i < arr.length; i++) {
+    var new_arr_tmp = arr[i];
+    if (Array.isArray(new_arr_tmp)) {
+      new_arr_tmp = arrayParser(new_arr_tmp);
+    }
+    new_arr = new_arr.concat(new_arr_tmp);
+  }
+  return new_arr;
+}
+
+const flattenArray_ES6 = 
+  arr => arr.reduce((accumulated, current_value) => accumulated.concat(Array.isArray(current_value) ? arrayParser(current_value) : current_value), []);
